9744f7a60f931bd9873fcd06ed78b67e26a3f03a
Jett Seale <sealee@oregonstate.edu>
2021-03-20 15:51:34 -0700

    Updated functions in check-commit.js

--- scripts/check-commit.js
+++ scripts/check-commit.js
@@ -1,41 +1,22 @@
 /* eslint-disable import/no-dynamic-require, no-console */
 const chalk = require('chalk');
 const path = require('path');
+
 const fetch = require('node-fetch');
 const simpleGit = require('simple-git/promise');
 
 const cwd = process.cwd();
 const git = simpleGit(cwd);
-
 const { version } = require(path.resolve(cwd, 'package.json'));
 
-function exitProcess(code = 1) {
-  console.log(''); // Keep an empty line here to make looks good~
+const starterCode = 1;
+function exitProcess(code = starterCode) {
   process.exit(code);
 }
 
-async function checkVersion() {
-  const { versions } = await fetch('http://registry.npmjs.org/antd').then(res => res.json());
-  if (version in versions) {
-    console.log(chalk.yellow('😈 Current version already exists. Forget update package.json?'));
-    console.log(chalk.cyan(' => Current:'), version);
-    exitProcess();
-  }
-}
-
 async function checkBranch({ current }) {
-  if (current !== 'master' && current !== '4.0-prepare') {
+  if (current !== '4.0-prepare' && current !== 'master') {
     console.log(chalk.yellow('🤔 You are not in the master branch!'));
-    exitProcess();
-  }
-}
-
-async function checkCommit({ files }) {
-  if (files.length) {
-    console.log(chalk.yellow('🙄 You forgot something to commit.'));
-    files.forEach(({ path: filePath, working_dir: mark }) => {
-      console.log(' -', chalk.red(mark), filePath);
-    });
     exitProcess();
   }
 }
@@ -50,15 +31,30 @@ async function checkRemote() {
   }
 }
 
+async function checkCommit({ files }) {
+  if (files.length !== 0) {
+    console.log(chalk.yellow('🙄 You forgot something to commit.'));
+    files.forEach(({ path: filePath, working_dir: mark }) => {
+      console.log(' -', chalk.red(mark), filePath);
+    });
+    exitProcess();
+  }
+}
+
+async function checkVersion() {
+  const { versions } = await fetch('http://registry.npmjs.org/antd').then(res => res.json());
+  if (version in versions) {
+    console.log(chalk.yellow('😈 Current version already exists. Forget update package.json?'));
+    console.log(chalk.cyan(' => Current:'), version);
+    exitProcess();
+  }
+}
+
 async function checkAll() {
   const status = await git.status();
-
   await checkVersion();
-
   await checkBranch(status);
-
   await checkCommit(status);
-
   await checkRemote();
 }
 
