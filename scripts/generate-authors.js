bb6bb9864ec99ca1033e4c600c30dd06b9b02540
Jett Seale <sealee@oregonstate.edu>
2021-03-20 15:18:13 -0700

    Made changes to generate-authors.js

--- scripts/generate-authors.js
+++ scripts/generate-authors.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const fs = require('fs');
-const simpleGit = require('simple-git/promise');
 const _ = require('lodash');
+const simpleGit = require('simple-git/promise');
+const fs = require('fs');
 
 const cwd = process.cwd();
 const git = simpleGit(cwd);
@@ -17,15 +17,19 @@ const excludes = [
 
 async function execute() {
   let logs = (await git.log()).all;
+
   logs = _.remove(logs, ({ author_email: email }) => {
-    for (let i = 0; i < excludes.length; i++) {
+    const len = excludes.length;
+    for (let i = 0; i < len; i++) {
       const item = excludes[i];
+
       if (email.indexOf(item) !== -1) {
         return false;
       }
     }
     return true;
   });
+
   logs = _.sortBy(_.unionBy(logs, 'author_email'), 'author_name');
   fs.writeFileSync(
     path.join(cwd, 'AUTHORS.txt'),
