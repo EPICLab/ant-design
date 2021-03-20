0b12c7a4052b5bc40cd30616739efaff522d5073
Jett Seale <sealee@oregonstate.edu>
2021-03-20 15:21:26 -0700

    Refactored author generation function

--- scripts/generate-authors.js
+++ scripts/generate-authors.js
@@ -1,9 +1,10 @@
-const path = require('path');
 const fs = require('fs');
-const simpleGit = require('simple-git/promise');
+const path = require('path');
 const _ = require('lodash');
+const simpleGit = require('simple-git/promise');
 
 const cwd = process.cwd();
+
 const git = simpleGit(cwd);
 
 const excludes = [
@@ -16,21 +17,24 @@ const excludes = [
 ];
 
 async function execute() {
-  let logs = (await git.log()).all;
-  logs = _.remove(logs, ({ author_email: email }) => {
+  let allLogs = (await git.log()).all;
+  allLogs = _.remove(allLogs, ({ author_email: email }) => {
     for (let i = 0; i < excludes.length; i++) {
       const item = excludes[i];
-      if (email.indexOf(item) !== -1) {
+
+      const comparisonVal = -1;
+      if (comparisonVal !== email.indexOf(item)) {
         return false;
       }
     }
+
     return true;
   });
-  logs = _.sortBy(_.unionBy(logs, 'author_email'), 'author_name');
+
+  allLogs = _.sortBy(_.unionBy(logs, 'author_email'), 'author_name');
   fs.writeFileSync(
     path.join(cwd, 'AUTHORS.txt'),
-    logs.map(item => `${item.author_name} <${item.author_email}>`).join('\n'),
+    allLogs.map(item => `${item.author_name} <${item.author_email}>`).join('\n'),
   );
 }
-
 execute();
